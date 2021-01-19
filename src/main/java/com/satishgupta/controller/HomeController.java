29b91b5ecabb6afb894fc6f1c0d0e592a895d2e1
Satish Kumar <satish.kumar06@nagarro.com>
2021-01-19 13:20:02 +0530

    Setup Bootstrap and template(thymeleaf)
    Signed-off-by: Satish Kumar <satish.kumar06@nagarro.com>

--- src/main/java/com/satishgupta/controller/HomeController.java
+++ src/main/java/com/satishgupta/controller/HomeController.java
@@ -1,33 +1,21 @@
 package com.satishgupta.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.satishgupta.dao.UserRepository;
-import com.satishgupta.entities.Contact;
-import com.satishgupta.entities.User;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
-	
-	@Autowired
-	private UserRepository userRepository;
-	
-	@GetMapping("/test")
-	@ResponseBody
-	public String test() {
-		User user = new User();
-		user.setName("Satish Gupta");
-		user.setEmail("satish@dev.io");
-		
-		Contact contact = new Contact();
-		
-		user.getContacts().add(contact);
-		
-		userRepository.save(user);
-		
-		return "Working";
+
+	@RequestMapping("/home")
+	public String home(Model model) {
+		model.addAttribute("title","Home - Smart Contact Manager");
+		return "home";
+	}
+
+	@RequestMapping("/about")
+	public String about(Model model) {
+		model.addAttribute("title","About - Smart Contact Manager");
+		return "about";
 	}
 }
